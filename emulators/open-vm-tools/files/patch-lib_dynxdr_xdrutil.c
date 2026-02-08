2eaeef0f83aecbc5d917d75e5c926be4d43ae881
mhannis <60040465+mhannis@users.noreply.github.com>
2026-02-08 00:01:04 -0500

    emulators/open-vm-tools: fix xdrproc_t call for FreeBSD 16

---
+++ emulators/open-vm-tools/files/patch-lib_dynxdr_xdrutil.c
@@ -0,0 +1,10 @@
+--- open-vm-tools/lib/dynxdr/xdrutil.c.orig	2025-09-03 01:35:59 UTC
++++ open-vm-tools/lib/dynxdr/xdrutil.c
+@@ -102,7 +102,7 @@
+    xdrmem_create(&xdrs, src, srcLen, XDR_DECODE);
+ 
+-   ret = (Bool) proc(&xdrs, dest, 0);
++   ret = (Bool) proc(&xdrs, dest);
+ 
+    xdr_destroy(&xdrs);
+ 
